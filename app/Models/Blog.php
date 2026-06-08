f1af786a9140dc5e489fbc35e1f5f08a82330ad4
donghuijiao <17761521352@163.com>
2026-06-08 18:18:16 +0800

    图片地址

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use QL\QueryList;
 use Illuminate\Database\Eloquent\Model;
 
@@ -130,10 +131,15 @@ class Blog extends Model
     }
 
     public function getContentAttribute() {
-        $value = $this->attributes['content'] ?? null;
+        $value = $this->attributes['content'] ?? '';
 
         if ($value) {
-            return str_replace('https://global-ec-1251174242.cos.ap-hongkong.myqcloud.com', '/image', $value);
+            if (Str::contains($value, 'nanjing')) {
+                return str_replace('https://cc-1251174242.cos.ap-nanjing.myqcloud.com', '/image', $value);
+            } elseif (Str::contains($value, 'hongkong')) {
+                return str_replace('https://global-ec-1251174242.cos.ap-hongkong.myqcloud.com', '/img', $value);
+            }
+
         }
 
         return $value;
@@ -143,7 +149,11 @@ class Blog extends Model
         $value = $this->attributes['head_img'] ?? null;
 
         if ($value) {
-            return str_replace('https://global-ec-1251174242.cos.ap-hongkong.myqcloud.com', '/image', $value);
+            if (Str::contains($value, 'nanjing')) {
+                return str_replace('https://cc-1251174242.cos.ap-nanjing.myqcloud.com', '/image', $value);
+            } elseif (Str::contains($value, 'hongkong')) {
+                return str_replace('https://global-ec-1251174242.cos.ap-hongkong.myqcloud.com', '/img', $value);
+            }
         }
 
         return $value;
